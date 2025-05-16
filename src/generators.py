a6caa0d1c7b158b36235a865e5f62eb9e457ea54
AKBAPEL <77396774+AKBAPEL@users.noreply.github.com>
2025-05-16 23:09:44 +0300

    formatting

--- src/generators.py
+++ src/generators.py
@@ -18,11 +18,13 @@ def sample_h1(n: int, beta: float = np.sqrt(0.5)) -> np.ndarray:
     Генерация n независимых Laplace(0,β)-выборок.
     """
     return laplace.rvs(loc=0.0, scale=beta, size=n)
-def sample_H2(n, l=2 / (3 ** 0.5)):
+
+
+def sample_H2(n, l=2 / (3**0.5)):
     """Генерация выборки из Exp(l) размера n."""
     return expon.rvs(loc=0, scale=l, size=n)
 
 
-def sample_H3(n,b=1.0,alpha=3):
+def sample_H3(n, b=1.0, alpha=3):
     """Генерация выборки из Pareto(a) размера n."""
-    return pareto.rvs(b,loc=0, scale=alpha, size=n)
+    return pareto.rvs(b, loc=0, scale=alpha, size=n)
