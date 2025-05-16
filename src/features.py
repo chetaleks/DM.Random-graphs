69a1419e1fb6343dd9ca5a5dad7438c1cf891be2
AKBAPEL <77396774+AKBAPEL@users.noreply.github.com>
2025-05-16 20:02:34 +0300

    Update features.py

--- src/features.py
+++ src/features.py
@@ -41,3 +41,43 @@ def compute_clique_number_1d(data: np.ndarray, d: float) -> int:
         # замена if size>max_clique на встроенный max
         max_clique = max(max_clique, size)
     return max_clique
+def count_connected_components(graph):
+    """
+    Вычисляет число компонент связности в неориентированном графе.
+
+    :param graph: Словарь, представляющий граф в виде смежности.
+                  Ключи - вершины, значения - списки соседей.
+    :return: Число компонент связности в графе.
+    """
+    visited = set()  # Множество для отслеживания посещенных вершин
+    component_count = 0  # Счетчик компонент связности
+
+    def dfs(vertex):
+        """Вспомогательная функция для выполнения DFS."""
+        stack = [vertex]
+        while stack:
+            current = stack.pop()
+            for neighbor in graph[current]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    stack.append(neighbor)
+
+    for vertex in graph:
+        if vertex not in visited:
+            # Если вершина не посещена, это новая компонента связности
+            visited.add(vertex)
+            dfs(vertex)
+            component_count += 1
+
+    return component_count
+
+
+
+
+# Функция для подсчета хроматического числа
+def compute_chromatic_number(G):
+    """Возвращает хроматическое число χ(G) графа G."""
+    # Используем жадную раскраску
+    coloring = nx.coloring.greedy_color(G, strategy="largest_first")
+    chromatic_number = len(set(coloring.values()))  # Количество уникальных цветов
+    return chromatic_number
